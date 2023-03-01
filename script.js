ad190df32c355240e6b2c5628cbdea6d3f8e32c7
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-03-01 15:07:17 -0300

    feat: creating a login/register page

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const title =document.querySelector('.title');
+const title = document.querySelector('.title');
+const cover = document.querySelector('.cover');
+const body = document.querySelector('body');
+const container = document.querySelector('.container');
+const darkThemeButton = document.querySelector('.btn-dark');
+const lightThemeButton = document.querySelector('.btn-light');
+const coverSub = document.querySelector('.coverSub');
 
-console.log(title.innerText);
-
-if (title.innerText == 'Quero ser trocado') {
-    title.innerHTML = 'Hello World';
+function lightMode() {
+    body.style.backgroundImage = 'url(background-gradient.svg)';
+    container.style.backgroundColor = '#f4f4f4';
+    container.style.color = '#000';
 }
 
-console.log(title.innerText);
+function darkMode() { 
+    body.style.backgroundImage = 'url(background-dark-gradient.svg)';
+    container.style.backgroundColor = '#242424';
+    container.style.color = '#fff';
+}
+
+function goToLogin() {
+    if(cover.classList.contains('toRegister')) {
+        cover.classList.remove('toRegister');
+    }
+    cover.classList.add('toLogin');
+    coverSub.innerText = 'Bora se logar?';
+}
+
+function goToRegister() {
+    if(cover.classList.contains('toLogin')) {
+        cover.classList.remove('toLogin');
+    }
+
+    coverSub.innerText = 'Bora se cadastrar?';
+}
